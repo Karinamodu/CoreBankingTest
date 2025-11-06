d6d02db227daf04dca719c6eace13bc14c4b0e43
Karinamodu <karinamodu@gmail.com>
2025-11-06 14:05:03 +0100

    day 7 work with gRPC

---
+++ CoreBanking.API/Hubs/Interfaces/IBankingClient.cs
@@ -0,0 +1,21 @@
+﻿using CoreBanking.API.Hubs.Models;
+using ConnectionState = CoreBanking.API.Hubs.Models.ConnectionState;
+
+namespace CoreBanking.API.Hubs.Interfaces
+{
+    public interface IBankingClient
+    {
+        // Transaction notifications
+        Task ReceiveTransactionNotification(TransactionNotification notification);
+        Task ReceiveBalanceUpdate(BalanceUpdate update);
+
+        // System alerts
+        Task ReceiveSystemAlert(SystemAlert alert);
+
+        // Fraud detection
+        Task ReceiveFraudAlert(FraudAlert alert);
+
+        // Connection management
+        Task ConnectionStateChanged(ConnectionState state);
+    }
+}
---
+++ CoreBanking.API/gRPC/Services/TradingGrpcService.cs
@@ -0,0 +1,18 @@
+﻿//using MediatR;
+
+//namespace CoreBanking.API.gRPC.Services
+//{
+//    public class TradingGrpcService : EnhancedAccountService.EnhancedAccountServiceBase
+//    {
+//        private readonly IMediator _mediator;
+//        private readonly ILogger<TradingGrpcService> _logger;
+
+//        public TradingGrpcService(IMediator mediator, ILogger<TradingGrpcService> logger)
+//        {
+//            _mediator = mediator;
+//            _logger = logger;
+//        }
+
+
+//    }
+//}
